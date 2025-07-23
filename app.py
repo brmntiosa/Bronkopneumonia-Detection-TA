c169b691fd9b3233bdacf06882a725de01f7d0f5
Bramantio Syahrul Alam <98206948+brmntiosa@users.noreply.github.com>
2025-07-23 23:16:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -101,4 +101,5 @@ artikel_data = [
 ]
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 5000))  # Railway akan set PORT otomatis
+app.run(host='0.0.0.0', port=port)
